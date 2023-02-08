3261c2e3d4f8d30ed651cfb7f2b2b1f04eeb4029
llorracc <git@llorracc.org>
2023-02-07 22:02:29 -0500

    First commit of old directory as repo

---
+++ DataAndPrograms/JavaSims/Inputted.java
@@ -0,0 +1,15 @@
+/**Class stores time series data for expectations history
+ * inputted into model
+ */
+
+public class Inputted{
+	public String time_desc;
+	public float inf;
+	
+	/**Constructor
+	 */
+	public Inputted(String t, float i){
+		time_desc=t;
+		inf=i;
+	}
+}
---
+++ EpidemiologyData_Unused/Stata_models_infl_q.do
@@ -0,0 +1,1 @@
+do Stata_make_infl_q.doreg adlcpimica adlcpispfm L.adlcpimica, noconstantpredict residuals adlcpimica_ewntestq adlcpimica_e
